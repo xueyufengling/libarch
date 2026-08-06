55add2f6ae9a8e0e1766b337b3b605cce79b9a51
xueyufengling <923611078@qq.com>
2026-08-06 22:44:36 +0800

    renamed build syscall

--- include/arch/syscall.h
+++ include/arch/syscall.h
@@ -61,7 +61,7 @@ typedef usyscall_t<syscall_ret_t> syscall_t;
  * 		  该内存必须有执行权限才能调用。
  */
 template<typename _RetType = long, typename _SyscallNo = long, typename _StackClean = unsigned short>
-usyscall_t<_RetType> syscall(syscall_type type, void* mem, _SyscallNo syscall_num, _StackClean syscall_clean = 0)
+usyscall_t<_RetType> build_syscall(syscall_type type, void* mem, _SyscallNo syscall_num, _StackClean syscall_clean = 0)
 {
 	memcpy(mem, syscall_tpl(type), syscall_size(type)); //每个调用号都有自己的syscall函数内存
 	*(_SyscallNo*)((char*)mem + syscall_no_offset(type)) = syscall_num; //替换模板中的syscall number
@@ -75,9 +75,9 @@ usyscall_t<_RetType> syscall(syscall_type type, void* mem, _SyscallNo syscall_nu
  * 		  直接使用syscall指令进行内存分配，如果操作系统ring3库被注入，此函数也不会受到影响，但如果内核层ring0被注入或者有驱动，此函数亦将受影响.
  * 		  内核层注入属于最危险的漏洞，操作系统开发者几乎肯定会在未来的新版本中修复，因此本库只考虑绕开ring3的注入。
  */
-inline syscall_t syscall(void* mem, syscall_no_t syscall_num, syscall_stack_clean_t syscall_clean = 0)
+inline syscall_t build_syscall(void* mem, syscall_no_t syscall_num, syscall_stack_clean_t syscall_clean = 0)
 {
-	return syscall<syscall_ret_t>(host_syscall_type, mem, syscall_num, syscall_clean);
+	return build_syscall<syscall_ret_t>(host_syscall_type, mem, syscall_num, syscall_clean);
 }
 
 }
